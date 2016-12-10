ea1c61045e022a3386d62b61094ba37eb59a86a5
wouzar <wouzar@gmail.com>
2016-12-10 18:33:18 +0200

    Initial commit

---
+++ DESEncryption/DESEncryption/Main.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace DESEncryption
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(12, 26);
+            this.inputText.Multiline = true;
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(100, 223);
+            this.inputText.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(135, 51);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(75, 23);
+            this.decryptButton.TabIndex = 2;
+            this.decryptButton.Text = "decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(236, 26);
+            this.outputText.Multiline = true;
+            this.outputText.Name = "outputText";
+            this.outputText.Size = new System.Drawing.Size(100, 223);
+            this.outputText.TabIndex = 3;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 261);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inputText);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.TextBox outputText;
+    }
+}
+
---
+++ DESEncryption/DESEncryption/Main.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace DESEncryption
+{
+    public partial class Main : Form
+    {
+
+        DESCryptoServiceProvider cryptic = null;
+        private static byte[] DESKey = { 0, 3, 5, 24, 53, 23, 4, 80 };
+        private static byte[] DESInitializationVector = { 0 , 43, 23, 53, 2, 53, 2 ,54 };
+
+
+        public Main()
+        {
+            InitializeComponent();
+
+            cryptic = new DESCryptoServiceProvider();
+            cryptic.Key = ASCIIEncoding.ASCII.GetBytes("DefaultK");
+            cryptic.IV = ASCIIEncoding.ASCII.GetBytes("DefaultV");
+            cryptic.Mode = CipherMode.CBC;
+    
+        }
+
+        public static string Encrypt(string value)
+        {
+            using (var cryptoProvider = new DESCryptoServiceProvider())
+            using (var memoryStream = new MemoryStream())
+            using (var cryptoStream = new CryptoStream(memoryStream, cryptoProvider.CreateEncryptor(DESKey, DESInitializationVector), CryptoStreamMode.Write))
+            using (var writer = new StreamWriter(cryptoStream))
+            {
+                writer.Write(value);
+                writer.Flush();
+                cryptoStream.FlushFinalBlock();
+                writer.Flush();
+                return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+            }
+        }
+
+        public static string Decrypt(string value)
+        {
+            using (var cryptoProvider = new DESCryptoServiceProvider())
+            using (var memoryStream = new MemoryStream(Convert.FromBase64String(value)))
+            using (var cryptoStream = new CryptoStream(memoryStream, cryptoProvider.CreateDecryptor(DESKey, DESInitializationVector), CryptoStreamMode.Read))
+            using (var reader = new StreamReader(cryptoStream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private string encodeString(string str)
+        {
+            byte[] data = ASCIIEncoding.ASCII.GetBytes(inputText.Text);
+            MemoryStream stream = new MemoryStream(data);
+            CryptoStream crStream = new CryptoStream(stream, cryptic.CreateEncryptor(), CryptoStreamMode.Read);
+
+            StreamReader reader = new StreamReader(crStream);
+            return reader.ReadToEnd();
+        }
+
+        private string decodeString(string str)
+        {
+            byte[] data = ASCIIEncoding.ASCII.GetBytes(inputText.Text);
+            MemoryStream stream = new MemoryStream(data);
+            CryptoStream crStream = new CryptoStream(stream, cryptic.CreateDecryptor(), CryptoStreamMode.Read);
+
+            StreamReader reader = new StreamReader(crStream);
+            return reader.ReadToEnd();
+        }
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            //FileStream fs = new FileStream(@"d:\test.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            outputText.Text = Encrypt(inputText.Text);
+
+
+
+
+            //crStream.Write(data, 0, data.Length);
+
+            //crStream.Close();
+
+
+
+            //fs.Close();
+        }
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+
+            outputText.Text = Decrypt(inputText.Text);
+            //cryptic = new DESCryptoServiceProvider();
+            //cryptic.Key = ASCIIEncoding.ASCII.GetBytes("Kekasuss");
+            //cryptic.IV = ASCIIEncoding.ASCII.GetBytes("Kekasuss");
+            //cryptic.Mode = CipherMode.CBC;
+
+            //FileStream fs = new FileStream(@"d:\test.txt", FileMode.Open, FileAccess.Read);
+
+            //CryptoStream crStream = new CryptoStream(fs, cryptic.CreateDecryptor(), CryptoStreamMode.Read);
+
+            //StreamReader reader = new StreamReader(crStream);
+
+            //string data = reader.ReadToEnd();
+
+            //MessageBox.Show(data);
+
+            //reader.Close();
+
+            //fs.Close();
+
+        }
+    }
+}
